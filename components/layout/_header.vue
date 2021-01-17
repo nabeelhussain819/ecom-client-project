03c7c3267cfc115b98388a46d6b295f634b4bf86
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-01-17 21:10:32 +0500

    minor ui changes

---
+++ components/layout/_header.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-layout-header class="clearfix">
+    <div class="logo" />
+
+    <a-menu
+      theme="dark"
+      class="header_nav"
+      mode="horizontal"
+      :style="{ lineHeight: '64px', float: right }"
+    >
+      <a-menu-item key="4" class="no-hover-nav">
+        <a-input default-value="Search">
+          <a-select slot="addonAfter" default-value="buy" style="width: 90px">
+            <a-select-option value="buy"> Buy </a-select-option>
+            <a-select-option value="sale"> Sale </a-select-option>
+          </a-select>
+        </a-input>
+      </a-menu-item>
+      <a-menu-item key="1">Login</a-menu-item>
+      <a-menu-item key="2">Sign Up</a-menu-item>
+      <a-menu-item key="3">+Post</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<style scoped>
+.header_nav {
+  float: right;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,17 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <a-layout id="components-layout-demo-top" class="layout">
+    <Header />
+    <a-layout-content>
+      <Nuxt />
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+<script>
+import Header from '~/components/layout/_header.vue'
+const components = { Header }
+export default { components }
+</script>
