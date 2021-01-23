67a5f506d2d41fc6aeda036b2695d6867e28b017
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-01-23 15:51:21 +0500

    remove unsuse code

--- Constant/index.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = process.env.baseUrl;
-export const BASE_API = process.env.api;
-export const CLIENT_ID = process.env.client_id;
-export const CLIENT_SECRET = process.env.client_secret;
-export const GRANT_TYPE = process.env.grant_type;
