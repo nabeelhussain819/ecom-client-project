f24508c93e1d8fef5b094f2c65f9dd2fa2fe478c
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-01-27 04:10:41 +0500

    minor change s

--- services/API/Product.js
+++ services/API/Product.js
@@ -1,47 +1,50 @@
 import request from '../request'
-import { BASE_API } from '../../Constant'
+import { BASE_API } from './../Constant'
 
-const baseUrl = `${BASE_API}products`;
+const baseUrl = `${BASE_API}products`
 
 function all(params = {}) {
-    return request({
-        url: baseUrl,
-        params
-    });
+  return request({
+    url: baseUrl,
+    params,
+  })
 }
 
-
 function save(data) {
-    return request({
-        url: baseUrl,
-        data,
-        method: 'POST',
-    });
+  return request({
+    url: baseUrl,
+    data,
+    method: 'POST',
+  })
 }
 
 function update(id, data) {
-    return request({
-        url: `${baseUrl}/${id}`,
-        data,
-        method: 'PATCH',
-    });
+  return request({
+    url: `${baseUrl}/${id}`,
+    data,
+    method: 'PATCH',
+  })
 }
 
 function get(id, params = {}) {
-    return request({
-        url: `${baseUrl}/show/${id}`,
-        params,
-    });
+  return request({
+    url: `${baseUrl}/show/${id}`,
+    params,
+  })
 }
 
 function destroy(id) {
-    return request({
-        url: `${baseUrl}/${id}`,
-        method: 'DELETE',
-    });
+  return request({
+    url: `${baseUrl}/${id}`,
+    method: 'DELETE',
+  })
 }
 const Products = {
-    all, save, update, get, destroy
-};
+  all,
+  save,
+  update,
+  get,
+  destroy,
+}
 
 export default Products
