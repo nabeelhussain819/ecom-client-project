b339d6f1c482865ab885edcea6f40b5cecf9223b
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-01-28 03:46:39 +0500

    remove unnessary code

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,14 @@
   <div class="container">
     <carousel />
     <masonry-list />
-    <a-modal
-      v-model="visible"
-      title="Add Products"
-      destroy-on-close
-      footer
-      width="80%"
-    >
-      <add-product></add-product>
-    </a-modal>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import addProduct from '~/components/products/AddProduct'
-import masonryList from '~/components/products/masonryList.vue'
+import MasonryList from '~/components/products/masonryList.vue'
 import Carousel from '~/components/UI/Carousel.vue'
-const components = { masonryList, Carousel, addProduct }
+const components = { MasonryList, Carousel }
 function showProductModal() {}
 
 function data() {
@@ -32,11 +22,9 @@ export default Vue.extend({
   method,
   data,
   created() {
-    const $this = this
-    this.$nuxt.$on('showProductModal', (a, b) => {
-      $this.visible = true
-      console.log(a, b)
-    })
+    // this.$nuxt.$on('showProductModal', (a, b) => {
+    //   $this.visible = true
+    // })
   },
 })
 </script>
