e9c4e0ef7ea1ee948d479e0d7886ad686f1e7399
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-01-29 03:23:03 +0500

    tile work and its component swtich work remaining

---
+++ components/cards/Tile.vue
@@ -0,0 +1,39 @@
+<template>
+  <a-card :loading="loading" hoverable @click="onClick">
+    <img slot="cover" :alt="title" :src="image" />
+    <a-card-meta :title="title">
+      <template slot="description">{{ description }} </template>
+    </a-card-meta>
+  </a-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    onClick() {
+      this.$emit('click')
+    },
+  },
+}
+</script>
