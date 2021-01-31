23355076c9c3fe7c2ae0d814816abe7197013e4a
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-02-01 04:25:50 +0500

    working on the flow

--- pages/post/category/_id.vue
+++ pages/post/category/_id.vue
@@ -1,9 +1,20 @@
 <template>
-  <product />
+  <a-card class="tiles-card dark-header" size="small" title="Choose A Category">
+    <list-menu @getCategoryId="getCategoryId" />
+  </a-card>
 </template>
 
 <script>
-import Product from '~/components/products'
-const components = { Product }
-export default { components }
+import ListMenu from '~/components/categories/ListMenu'
+const components = { ListMenu }
+function getCategoryId(categoryId) {
+  this.$router.push({ path: `/post/product/create/${categoryId}` })
+  this.showProduct = true
+}
+
+const methods = {
+  getCategoryId,
+}
+
+export default { components, methods }
 </script>
---
+++ pages/post/product/create/_id.vue
@@ -0,0 +1,10 @@
+<template>
+  <add-product />
+</template>
+<script>
+import AddProduct from '~/components/products/AddProduct'
+const components = {
+  AddProduct,
+}
+export default { components }
+</script>
