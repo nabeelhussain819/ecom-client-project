8fbd126464667a29150aca079a3f7cac317a7962
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-02-03 00:55:00 +0500

    remove legacy code

--- store/index.js
+++ store/index.js
@@ -1,48 +1,15 @@
 import Vuex from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
+
 import location from './modules/location'
 import auth from './modules/auth'
 export const strict = false
 
 const store = () =>
   new Vuex.Store({
-    state: {
-      status: false,
-      token: null,
-
-      language: null,
-      currentLocation: null,
-      searchParams: {},
-    },
-    mutations: {
-      authStatus(state, payload) {
-        state.status = payload.status
-        state.token = payload.token
-      },
-
-      setCurrentLocation(state, payload) {
-        state.currentLocation = payload
-      },
-      setSearchParams(state, payload) {
-        state.searchParams = payload
-      },
-    },
+    state: {},
+    mutations: {},
     actions: {},
-
-    getters: {
-      isAuthenticated(state) {
-        return state.status
-      },
-      getCurrentLocation(state) {
-        return state.currentLocation
-      },
-      getUserDetails(state) {
-        return state.user
-      },
-      getSearchParams(state) {
-        return state.searchParams
-      },
-    },
+    getters: {},
     modules: { location, auth },
   })
 
