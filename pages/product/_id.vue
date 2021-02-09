ee12dd1f917b393e8e9d1314ddef95994c9b4e32
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-02-10 02:52:56 +0500

    fix:: remove comment

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -13,22 +13,17 @@
           {{ product.description }}
         </a-col>
         <a-col :xs="24" :sm="6"> <owner-detail :product="product" /> </a-col>
-      </a-row>
-      <a-divider></a-divider>
-      <h2 class="heading">Add Comment</h2>
-      <comments /></div
+      </a-row></div
   ></a-skeleton>
 </template>
 <script>
 import Product from '~/services/API/Product'
-import comments from '~/components/comments'
 import imageSlider from '~/components/sliders/ImageSlider'
 import OwnerDetail from '~/components/products/OwnerDetail'
 import { isEmpty } from '~/services/Utilities'
 
 export default {
   components: {
-    comments,
     OwnerDetail,
     imageSlider,
   },
