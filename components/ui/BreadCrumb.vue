b0d2a39ba387fc0b57d4d46cf6b8a69b45d638c1
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-07-11 14:26:49 +0500

    fix: adding with change the name of the folders

---
+++ components/ui/BreadCrumb.vue
@@ -0,0 +1,8 @@
+<template>
+  <a-breadcrumb>
+    <a-breadcrumb-item>Home</a-breadcrumb-item>
+    <!-- <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+      <a-breadcrumb-item><a href="">Application List</a></a-breadcrumb-item> -->
+    <a-breadcrumb-item>Selected Category</a-breadcrumb-item>
+  </a-breadcrumb>
+</template>
---
+++ components/ui/Carousel.vue
@@ -0,0 +1,44 @@
+<template>
+  <a-carousel :after-change="onChange">
+    <div>
+      <img
+        id="slider-image"
+        src="https://cdn.singpromos.com/wp-content/uploads/2020/11/Huawei-26-Nov-2020.jpg"
+      />
+    </div>
+    <div>
+      <img
+        id="slider-image"
+        src="https://www.brandsynario.com/wp-content/uploads/2019/03/S10-Galaxy.jpg"
+      />
+    </div>
+  </a-carousel>
+</template>
+<script>
+export default {
+  methods: {
+    // onChange(a, b, c) {
+    //   console.log(a, b, c)
+    // },
+  },
+}
+</script>
+<style scoped>
+/* For demo */
+.ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 360px;
+  line-height: 160px;
+  background: #364d79;
+  overflow: hidden;
+}
+
+.ant-carousel >>> .slick-slide h3 {
+  color: #fff;
+}
+
+#slider-image {
+  width: 100%;
+  height: fit-content;
+}
+</style>
