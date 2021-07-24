491c4bb77584f0e3eff19531c344db09e62e2f4a
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-07-24 23:00:14 +0500

    feat: styling change

--- components/layout/_header.vue
+++ components/layout/_header.vue
@@ -6,12 +6,15 @@
       mode="horizontal"
       :style="{ lineHeight: '64px', float: 'right' }"
     >
-      <a-menu-item key="5" class="no-hover-nav">
-        <a-input id="search" placeholder="Search" @pressEnter="search">
-          <a-select slot="addonAfter" default-value="buy" style="width: 90px">
-            <a-select-option value="buy"> Buy </a-select-option>
-            <a-select-option value="sale"> Sale </a-select-option>
-          </a-select>
+      <a-menu-item key="5" class="no-hover-nav search-bar">
+        <a-input id="search-input" placeholder="Search" @pressEnter="search">
+          <template slot="addonAfter" default-value="buy" style="width: 90px">
+            <a-button type="primary"> Buying </a-button>
+            <a-divider type="vertical" />
+            <a-button type="primary"> Services </a-button>
+            <a-divider type="vertical" />
+            <a-button type="primary"> Go </a-button>
+          </template>
         </a-input>
       </a-menu-item>
       <a-menu-item key="1">
