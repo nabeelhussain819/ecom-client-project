f782a65b78dac82d6e4c840b738829a24ca12ae2
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-12-11 02:30:07 +0500

    fix: websocket work

--- plugins/echo.js
+++ plugins/echo.js
@@ -5,14 +5,14 @@ window.Pusher = require('pusher-js')
 window.Echo = new Echo({
   broadcaster: 'pusher',
   key: 'ABCD',
-  wsHost: '127.0.0.1',
+  wsHost: 'web-api.letsdeploy.us',
   encrypted: true,
   wsPort: 6001,
   wssPort: 6001,
-  enabledTransports: ['ws'],
+  enabledTransports: ['ws', 'wss'],
   disableStats: true,
   forceTLS: false,
-  authEndpoint: 'https://127.0.0.1/broadcasting/auth',
+  authEndpoint: 'http://web-api.letsdeploy.us/broadcasting/auth',
   auth: {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('access_token')}`,
