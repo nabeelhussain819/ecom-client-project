68972692999ae237eec882f607dd8dfcc58504e5
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2022-02-04 03:54:31 +0500

    feat: slider adding into the current store

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container">
+    <div class="mb-2">
+      <slider
+        :images="[
+          'https://galaxy.pk/wp-content/uploads/2022/01/Apple-Macbook-Pro-6.jpg',
+          'https://galaxy.pk/wp-content/uploads/2022/01/Apple-Macbook-Pro-6.jpg',
+        ]"
+      />
+    </div>
     <a-spin :spinning="loading">
       <a-row :gutter="16">
         <a-col v-for="(product, index) in products" :key="index" :span="3">
@@ -16,7 +24,8 @@ import GoogleAuth from '@/plugins/google-auth'
 import { FACEBOOK_APP_ID, GOOGLE_CLIENT_ID } from '~/services/Constant'
 import Products from '~/services/API/ProductServices'
 import Tile from '~/components/products/Tile'
-const components = { Tile }
+import slider from '~/components/sliders/ImageSlider'
+const components = { Tile, slider }
 
 // move to login component
 Vue.use(GoogleAuth, {
