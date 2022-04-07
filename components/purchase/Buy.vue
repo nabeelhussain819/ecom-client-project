6336a1f6ff4a95ce54ffb4ef2ebf27a72d24c849
nasib <mnnasib7777@gmail.com>
2022-04-08 04:35:17 +0500

    feat: adding purchase page

---
+++ components/purchase/Buy.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="container container-buy-now">
+    <h1>Confirm Purchase</h1>
+    <a-row :gutter="16">
+      <a-col :xs="24" :sm="24" class="">
+        <image-slider class="purchase_img_slider" :images="images" />
+        <h2 type="inline " :title="product.name" class="primary-text buy-price">
+          {{ product.name }}
+        </h2>
+        <div class="purchase-content">
+          <hr />
+          <div class="shipping-option">
+            <h2>Shipping to (required)</h2>
+            <a class="btn-add" @click="goto('/purchase/add-shipping/')">Add</a>
+          </div>
+          <hr />
+          <div class="payment-method-option">
+            <h2>Payment method (required)</h2>
+            <a class="btn-add" @click="goto('/purchase/payment-method/')"
+              >Add</a
+            >
+          </div>
+          <hr />
+          <div class="item-price-tag">
+            <h2>Item Price</h2>
+            <h2>${{ product.price }}</h2>
+          </div>
+          <div class="shipping-price-tag">
+            <h2>Shipping Price</h2>
+            <h2>$7.00</h2>
+          </div>
+          <div class="sales-tax-tag">
+            <h2>Sales Tax (Estimated)</h2>
+            <h2>$7.00</h2>
+          </div>
+          <div class="payed-tag">
+            <h2><b> You Pay</b></h2>
+            <h2><b> $207.00 </b></h2>
+          </div>
+          <div class="protect-payment-main">
+            <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN5NEy1Is3DEFYZM5YW4kcbRrTILo9ddmyJisQrqws5ylSUlNagQA5OaKQ6cN7BdvHRDw&usqp=CAU"
+                width="100px"
+              />
+            </div>
+            <div>
+              <h2>2-Days Purchase Protection</h2>
+              <p>
+                Items shipped through FlexEmarket come with a 2-day purchase
+                protection. <a href="javascript:;"> Learn more </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="confirm-purchase-main" style="text-align: center">
+          <button class="btn ant-btn ant-btn-primary ant-btn-lg">
+            Confirm Purchase
+          </button>
+          <div>
+            <p class="label-confirm-purchase">
+              By tapping "Confirm Purchase", you agree to the FlexEmarket
+              <a href="javascript:;"> Term of Service</a>
+            </p>
+          </div>
+        </div>
+      </a-col></a-row
+    >
+  </div>
+</template>
+<script>
+import imageSlider from '~/components/sliders/ImageSlider'
+import { isEmpty } from '~/services/Utilities'
+import Product from '~/services/API/ProductServices'
+import routeHelpers from '~/mixins/route-helpers'
+export default {
+  components: {
+    imageSlider,
+  },
+  mixins: [routeHelpers],
+  data() {
+    return { loading: false, product: {}, images: [], values: {} }
+  },
+  mounted() {
+    this.getProduct(this.$route.params.id)
+  },
+  methods: {
+    getProduct(id) {
+      this.loading = true
+      Product.get(id)
+        .then((product) => {
+          this.product = product
+          this.getImages(product)
+
+          product.products_attributes.map(
+            (a) => (this.values[a.attribute_id] = a.value)
+          )
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    getImages(product) {
+      if (!isEmpty(product.media)) {
+        const tempImage = []
+        product.media.map((images) => tempImage.push(images.url))
+        this.images = tempImage
+        return
+      }
+
+      this.images = [
+        'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png',
+      ]
+    },
+  },
+}
+</script>
