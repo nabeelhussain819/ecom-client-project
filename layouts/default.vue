b5265a86fc4b34cb91d0d9998f097120e3152291
nasib <mnnasib7777@gmail.com>
2022-08-01 01:48:12 +0300

    fix: header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
   <a-layout id="components-layout-demo-top" class="layout">
-    <!-- <Header /> -->
+    <Header />
     <a-layout-content class="main-container">
-      <!-- <Nuxt /> -->
+      <client-only>
+        <Nuxt />
+      </client-only>
     </a-layout-content>
     <NewsLetter />
     <Footer />
@@ -10,11 +12,11 @@
 </template>
 
 <script>
-// import Header from '~/components/layout/_header.vue'
+import Header from '~/components/layout/_header.vue'
 import NewsLetter from '~/components/layout/_newsletter.vue'
 import Footer from '~/components/layout/_footer.vue'
 const components = {
-  // Header,
+  Header,
   NewsLetter,
   Footer,
 }
