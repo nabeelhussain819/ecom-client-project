7231b6ceb62a49dd3026079ece717714f079a0a7
nasib <mnnasib7777@gmail.com>
2022-09-04 01:43:47 +0300

    fix: chanel private

--- components/layout/_header.vue
+++ components/layout/_header.vue
@@ -406,7 +406,7 @@ export default {
       }
     },
     catchEvent() {
-      window.Echo.channel('channel-test').listen('NewMessage', (e) => {
+      window.Echo.private('channel-test').listen('NewMessage', (e) => {
         console.log('channel-test')
         console.log(e)
       })
