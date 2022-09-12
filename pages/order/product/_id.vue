88bc0f2cf8f27e718f5a066871bcaf2afe541f85
nasib <mnnasib7777@gmail.com>
2022-09-12 23:04:53 +0300

    fix: adding link

--- pages/order/product/_id.vue
+++ pages/order/product/_id.vue
@@ -118,7 +118,11 @@
                       <td><h2>Status</h2></td>
                       <td class="td-100">
                         <i>Pending Shipment</i>
-                        <p class="primary-text">Track our item?</p>
+                        <nuxt-link
+                          to="https://www.fedex.com/en-us/tracking.html"
+                        >
+                          <p class="primary-text">Track our item?</p>
+                        </nuxt-link>
                       </td>
                     </tr>
                   </tbody>
